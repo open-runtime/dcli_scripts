6140043a36a2c328e828865b2428ef835f4bb25d
Tsavo Knott <tsavo@pieces.app>
2024-03-14 17:45:42 -0400

    All Composing Properly - TODO Ensure CI/CD - Prepare Module Template Generation Manager

--- bin/dwhich.dart
+++ bin/dwhich.dart
@@ -1,169 +1,157 @@
-#! /usr/bin/env dart
-/* Copyright (C) S. Brett Sutton - All Rights Reserved
- * Unauthorized copying of this file, via any medium is strictly prohibited
- * Proprietary and confidential
- * Written by Brett Sutton <bsutton@onepub.dev>, Jan 2022
- */
-
-// TODO: CRITICAL IMPLEMENTATION - https://github.com/onepub-dev/dcli_scripts/issues/3
-
-// The primary purpose of this code snippet is to search for a specific
-// application (referred to as 'appname') within the directories listed in the
-// PATH environment variable of the system. It aims to determine whether the
-// specified application exists and if it is executable. The process involves
-// iterating through the PATH directories, validating each path, and checking
-// for the presence and executability of the application. If the application is
-// found and is executable, the program prints the path to the application.
-// Additionally, the code handles various validations and error conditions,
-// such as empty paths or paths that do not exist, and reports any problems
-// detected during the search process.
-
-import 'dart:io';
-
-import 'package:dcli/dcli.dart';
-import 'package:args/args.dart' show ArgParser, ArgResults;
-import 'package:dcli_sdk/dcli_sdk.dart' show ExitException;
-import 'package:path/path.dart';
-
-/// dwhich appname - searches for 'appname' on the path
-void main(List<String> cliArgs) {
-  final ArgResults args;
-  var found = false;
-  var exectuable = false;
-
-  final parser = ArgParser()
-    ..addFlag(
-      'verbose',
-      abbr: 'v',
-      negatable: false,
-      help: 'Logs additional details to the cli',
-    );
-
-  final parsed = parser.parse(args);
-
-  try {
-    args = ArgParser().parse(cliArgs);
-
-    if (parsed.wasParsed('verbose')) {
-      Settings().setVerbose(enabled: true);
-    }
-
-    // validation(args, () => 'Path: ${env['PATH']}');
-
-    final paths = dedupPaths(args);
-
-    String? priorPath;
-
-    for (final path in paths) {
-      // validation(args, () => 'Searching: ${truepath(path)}');
-      if (!validatePath(path, args, priorPath)) {
-        continue;
-      }
-
-      final pathToCmd = containsCommand(truepath(path, args.command));
-      if (pathToCmd != null) {
-        found = true;
-        if (!isExecutable(pathToCmd)) {
-          print('Found at: $pathToCmd but it is not executable.');
-          continue;
-        }
-        exectuable = true;
-
-        print('Found at: $pathToCmd');
-
-        // found an exectuable.
-        if (args.first) {
-          break;
-        }
-      }
-      priorPath = path;
-    }
-  } on ExitException catch (e) {
-    printerr(red(e.message));
-    if (e.exitCode == ExitException.invalidArgs) {
-      Args.showUsage();
-    }
-    exit(e.exitCode);
-  }
-  final goodPath = printProblems(args);
-
-  exit(ExitException.mapExitCode(found: found, exectuable: exectuable, goodPath: goodPath));
-}
-
-bool validatePath(String path, Args args, String? lastPath) {
-  var valid = true;
-  var _path = path;
-  // validation(args, () => 'Searching: ${truepath(path)}');
-  if (_path.isEmpty) {
-    validation(args, () => 'Empty path found');
-    if (Platform.isLinux) {
-      _path = '.';
-
-      /// current
-      validation(
-          args,
-          () => orange('WARNING: current directory is on your path due '
-              'to an empty path '
-              '${lastPath == null ? '' : 'after $lastPath'} .'));
-    } else {
-      validation(args, () => red(
-          // ignore: lines_longer_than_80_chars
-          'Found empty path ${lastPath == null ? '' : 'after $lastPath'}.'));
-      valid = false;
-    }
-  }
-
-  if (valid && !exists(_path)) {
-    validation(args, () => red('The path $_path does not exist.'));
-    valid = false;
-  }
-  return valid;
-}
-
-/// Returns true if there we no problems dectected with PATH
-bool printProblems(Args args) {
-  if (problems.isNotEmpty) {
-    print('');
-    print(orange('Problems:'));
-    for (final problem in problems) {
-      printerr('    ${orange(problem)}');
-    }
-  }
-  return problems.isEmpty;
-}
-
-/// reports any duplicate path entries.
-Set<String> dedupPaths(Args args) {
-  final paths = <String>{};
-
-  for (final path in PATH) {
-    if (paths.contains(path)) {
-      validation(args, () => orange('Found duplicated path: $path in PATH'));
-    } else {
-      paths.add(path);
-    }
-  }
-  return paths;
-}
-
-String? containsCommand(String cmd) {
-  if (!Platform.isWindows || extension(cmd).isNotEmpty) {
-    return exists(cmd) ? cmd : null;
-  }
-
-  for (final ext in env['PATHEXT']!.split(';')) {
-    final fullCmd = '$cmd$ext';
-    if (exists(fullCmd)) {
-      return fullCmd;
-    }
-  }
-  return null;
-}
-
-final problems = <String>[];
-// ignore: avoid_positional_boolean_parameters
-void validation(Args args, String Function() message) {
-  if (args.validate) {
-    problems.add(message());
-  }
-}
+// #! /usr/bin/env dart
+// /* Copyright (C) S. Brett Sutton - All Rights Reserved
+//  * Unauthorized copying of this file, via any medium is strictly prohibited
+//  * Proprietary and confidential
+//  * Written by Brett Sutton <bsutton@onepub.dev>, Jan 2022
+//  */
+//
+// // TODO: CRITICAL IMPLEMENTATION - https://github.com/onepub-dev/dcli_scripts/issues/3
+//
+// // The primary purpose of this code snippet is to search for a specific
+// // application (referred to as 'appname') within the directories listed in the
+// // PATH environment variable of the system. It aims to determine whether the
+// // specified application exists and if it is executable. The process involves
+// // iterating through the PATH directories, validating each path, and checking
+// // for the presence and executability of the application. If the application is
+// // found and is executable, the program prints the path to the application.
+// // Additionally, the code handles various validations and error conditions,
+// // such as empty paths or paths that do not exist, and reports any problems
+// // detected during the search process.
+//
+// import 'dart:io';
+//
+// import 'package:dcli/dcli.dart';
+// import 'package:dcli_scripts/src/dwhich/args.dart';
+// import 'package:dcli_scripts/src/dwhich/exit_exception.dart';
+// import 'package:path/path.dart';
+//
+// /// dwhich appname - searches for 'appname' on the path
+// void main(List<String> cliArgs) {
+//   final Args args;
+//   var found = false;
+//   var exectuable = false;
+//
+//   try {
+//     args = Args.parse(cliArgs);
+//
+//     Settings().setVerbose(enabled: args.debug);
+//
+//     // validation(args, () => 'Path: ${env['PATH']}');
+//
+//     final paths = dedupPaths(args);
+//
+//     String? priorPath;
+//
+//     for (final path in paths) {
+//       // validation(args, () => 'Searching: ${truepath(path)}');
+//       if (!validatePath(path, args, priorPath)) {
+//         continue;
+//       }
+//
+//       final pathToCmd = containsCommand(truepath(path, args.command));
+//       if (pathToCmd != null) {
+//         found = true;
+//         if (!isExecutable(pathToCmd)) {
+//           print('Found at: $pathToCmd but it is not executable.');
+//           continue;
+//         }
+//         exectuable = true;
+//
+//         print('Found at: $pathToCmd');
+//
+//         // found an exectuable.
+//         if (args.first) {
+//           break;
+//         }
+//       }
+//       priorPath = path;
+//     }
+//   } on ExitException catch (e) {
+//     printerr(red(e.message));
+//     if (e.exitCode == ExitException.invalidArgs) {
+//       Args.showUsage();
+//     }
+//     exit(e.exitCode);
+//   }
+//   final goodPath = printProblems(args);
+//
+//   exit(ExitException.mapExitCode(found: found, exectuable: exectuable, goodPath: goodPath));
+// }
+//
+// bool validatePath(String path, Args args, String? lastPath) {
+//   var valid = true;
+//   var _path = path;
+//   // validation(args, () => 'Searching: ${truepath(path)}');
+//   if (_path.isEmpty) {
+//     validation(args, () => 'Empty path found');
+//     if (Platform.isLinux) {
+//       _path = '.';
+//
+//       /// current
+//       validation(
+//           args,
+//           () => orange('WARNING: current directory is on your path due '
+//               'to an empty path '
+//               '${lastPath == null ? '' : 'after $lastPath'} .'));
+//     } else {
+//       validation(args, () => red(
+//           // ignore: lines_longer_than_80_chars
+//           'Found empty path ${lastPath == null ? '' : 'after $lastPath'}.'));
+//       valid = false;
+//     }
+//   }
+//
+//   if (valid && !exists(_path)) {
+//     validation(args, () => red('The path $_path does not exist.'));
+//     valid = false;
+//   }
+//   return valid;
+// }
+//
+// /// Returns true if there we no problems dectected with PATH
+// bool printProblems(Args args) {
+//   if (problems.isNotEmpty) {
+//     print('');
+//     print(orange('Problems:'));
+//     for (final problem in problems) {
+//       printerr('    ${orange(problem)}');
+//     }
+//   }
+//   return problems.isEmpty;
+// }
+//
+// /// reports any duplicate path entries.
+// Set<String> dedupPaths(Args args) {
+//   final paths = <String>{};
+//
+//   for (final path in PATH) {
+//     if (paths.contains(path)) {
+//       validation(args, () => orange('Found duplicated path: $path in PATH'));
+//     } else {
+//       paths.add(path);
+//     }
+//   }
+//   return paths;
+// }
+//
+// String? containsCommand(String cmd) {
+//   if (!Platform.isWindows || extension(cmd).isNotEmpty) {
+//     return exists(cmd) ? cmd : null;
+//   }
+//
+//   for (final ext in env['PATHEXT']!.split(';')) {
+//     final fullCmd = '$cmd$ext';
+//     if (exists(fullCmd)) {
+//       return fullCmd;
+//     }
+//   }
+//   return null;
+// }
+//
+// final problems = <String>[];
+// // ignore: avoid_positional_boolean_parameters
+// void validation(Args args, String Function() message) {
+//   if (args.validate) {
+//     problems.add(message());
+//   }
+// }
